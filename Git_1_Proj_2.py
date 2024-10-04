229a0c94c38abcaaf24213f0a81a26ef9772eef5
VladimirGitPyth <vladimir.dubinin@gmail.com>
2024-10-04 16:41:48 +0400

    Первый Commit

---
+++ Git_1_Proj_2.py
@@ -0,0 +1,33 @@
+# Программа-калькулятор для изучения возможностей Git
+def sum(val1, val2):
+    print("Сумма равна: ", a + b)
+def razn(val1, val2):
+    print("Разность равна: ", a - b)
+def umn(val1, val2):
+    print("Произведение равно: ", a * b)
+def delen(val1, val2):
+    print("Результат деления: ", a / b)
+
+while True:
+    try:
+        a = int(input ("Введите первое число: "))
+        b = int(input ("Введите второе число: "))
+        c = input("Введите знак действия ('+', '-', '/', '*'): ")
+        if c == "*":
+            umn(a,b)
+            break
+        elif c=="+":
+            sum(a,b)
+            break
+        elif c=="-":
+            razn(a,b)
+            break
+        elif c=="/":
+            delen(a,b)
+            break
+        else:
+            print("Ошибка: ввели неправильный знак действия!")
+    except ZeroDivisionError:
+        print("Ошибка: деление на ноль!")
+    except ValueError:
+        print("Ошибка: ввели символ!")
